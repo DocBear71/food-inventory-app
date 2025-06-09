120d552cc1712c8122fb60d58c8eae60a74f363b
Edward McKeown <edward@doctormckeown.com>
2025-06-08 21:56:47 -0500

    fixing mobile first PWA 6-8-2025

--- src/components/layout/MobileDashboardLayout.js
+++ src/components/layout/MobileDashboardLayout.js
@@ -161,7 +161,7 @@ export default function MobileDashboardLayout({ children }) {
             )}
 
             {/* Main Content - Fixed padding to clear header */}
-            <main className={`pt-20 ${bottomPadding}`}>
+            <main className={`pt-70 ${bottomPadding}`}>
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                     {children}
                 </div>
