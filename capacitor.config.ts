41f936d561e49ea51e85d0535eeb23d0acc16128
Edward McKeown <edward@doctormckeown.com>
2025-06-16 21:01:17 -0500

    auth redirect 6-16-2025

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'kitchen.docbearscomfort',
+  appId: 'kitchen.docbearscomfort.www',
   appName: "Doc Bear's Comfort Kitchen",
   webDir: 'out',
     server: {
