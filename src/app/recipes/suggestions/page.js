3a02849277d5ea72f968ace6c749b7c12865a016
Edward McKeown <edward@doctormckeown.com>
2025-06-27 14:06:21 -0500

    fixing button issues 6-27-2025

--- src/app/recipes/suggestions/page.js
+++ src/app/recipes/suggestions/page.js
@@ -1082,7 +1082,7 @@ export default function RecipeSuggestions() {
                                                             >
                                                                 {loadingRecipe ? 'Loading...' : 'View Recipe'}
                                                             </TouchEnhancedButton>
-                                                            {!recipe.analysis.canMake && (
+                                                            {recipe.analysis.matchPercentage < 100 && (
                                                                 <TouchEnhancedButton
                                                                     onClick={() => setShowShoppingList({
                                                                         recipeId: recipe._id,
