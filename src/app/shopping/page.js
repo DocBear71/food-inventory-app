fae2bdbfcbadc577912ac56b3f701d3f8db4583a
Edward McKeown <edward@doctormckeown.com>
2025-07-19 22:11:34 -0500

    Moving from UnifiedShoppingListModal to EnhancedIAShoppingListModal 2 7-19-2025

--- src/app/shopping/page.js
+++ src/app/shopping/page.js
@@ -13,7 +13,6 @@ import { useSubscription, useFeatureGate } from '@/hooks/useSubscription';
 import FeatureGate, { SubscriptionIndicator } from '@/components/subscription/FeatureGate';
 import { FEATURE_GATES } from '@/lib/subscription-config';
 import { apiGet, apiPost } from '@/lib/api-config';
-import EnhancedAIShoppingListModal from "@/components/shopping/EnhancedAIShoppingListModal.js";
 
 export default function ShoppingPage() {
     const {data: session, status} = useSafeSession();
