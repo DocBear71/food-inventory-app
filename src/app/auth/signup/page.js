8e2bd5aa8dc26bd95e8ee0547ae4865082366b0e
Edward McKeown <edward@doctormckeown.com>
2025-09-05 14:51:01 -0500

    re-fixing a mistake on signup/page.js 3 9-5-2025

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -1161,7 +1161,7 @@ function SignUpContent() {
                                     <div className="space-y-2">
                                         <h4 className="font-semibold text-gray-900 text-sm">What's Included:</h4>
                                         <ul className="space-y-1">
-                                            {tier.features.slice(0, 6).map((feature, index) => (
+                                            {tier.features.map((feature, index) => (
                                                 <li key={index} className="flex items-start space-x-2">
                                                     <svg className="w-4 h-4 text-green-500 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                                                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -1169,11 +1169,6 @@ function SignUpContent() {
                                                     <span className="text-sm text-gray-700">{feature.name}</span>
                                                 </li>
                                             ))}
-                                            {tier.features.length > 6 && (
-                                                <li className="text-sm text-gray-500 pl-6">
-                                                    + {tier.features.length - 6} more features
-                                                </li>
-                                            )}
                                         </ul>
                                     </div>
                                 </div>
